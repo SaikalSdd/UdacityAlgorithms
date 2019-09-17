613f6766a01500ffff60637cbe2c30dc43933280
SaikalSdd <38737621+SaikalSdd@users.noreply.github.com>
2019-09-17 05:31:00 -0700

    Create excitement_show.py

---
+++ excitement_show.py
@@ -0,0 +1,17 @@
+# Write a function called "show_excitement" where the string
+# "I am super excited for this course!" is returned exactly
+# 5 times, where each sentence is separated by a single space.
+# Return the string with "return".
+# You can only have the string once in your code.
+# Don't just copy/paste it 5 times into a single variable!
+
+
+def show_excitement():
+    sentence = []
+    text = 'I am super excited for this course!'
+    for i in range(5):
+        sentence.append(text)
+    return "".join(sentence)
+
+print show_excitement()
+#output: I am super excited for this course!I am super excited for this course!I am super excited for this course!I am super excited for this course!I am super excited for this course!
